6fa5f9489116797974e9b828c2ab966d40dabc8d
Tharindu Manoj Thilakarathna <thilakarathnawatm.21@uom.lk>
2024-06-30 00:18:05 +0530

    chat is connected to whatsapp

---
+++ eventify-backend/Controllers/ChatController.cs
@@ -0,0 +1,42 @@
+﻿using eventify_backend.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace eventify_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChatController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public ChatController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        [HttpGet("{soRId}")]
+        public async Task<IActionResult> GetPhoneNumber([FromRoute] int soRId)
+        {
+            try
+            {
+                var item = await _appDbContext.ServiceAndResources.Include(s=> s.Vendor).FirstOrDefaultAsync(s=> s.SoRId == soRId);
+
+                var Phone = item!.Vendor?.Phone;
+
+                if(Phone == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(new { Phone = Phone});
+            }
+
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
--- eventify-backend/obj/Debug/net8.0/eventify-backend.AssemblyInfo.cs
+++ eventify-backend/obj/Debug/net8.0/eventify-backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("eventify-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+579a58f22d0f4c75dac2fc5db3d26c870996153a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8161a148c8763ce8e29eeef1a082cf291a9e8670")]
 [assembly: System.Reflection.AssemblyProductAttribute("eventify-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("eventify-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
